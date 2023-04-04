44f070b915b8cb53005eec4a98d653b2e4ca426b
Invasion <invasion@deeplay.io>
2023-04-04 18:55:03 +0700

    Add CharactersCollection class implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numberCollection = new NumbersCollection([4, -300, 110, 2]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
 console.log(sorter);
+
+const charactersCollection = new CharactersCollection('ZxYmba');
+const sorter1 = new Sorter(charactersCollection);
+sorter1.sort();
+console.log(sorter1);
