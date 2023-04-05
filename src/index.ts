0c9468a998d7874f194c56fc8950d219f8668ed4
Invasion <invasion@deeplay.io>
2023-04-05 16:16:00 +0700

    Add abstract class Sorter implementation

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,17 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numberCollection = new NumbersCollection([4, -300, 110, 2]);
-const sorter = new Sorter(numberCollection);
-sorter.sort();
-console.log(sorter);
+numberCollection.sort();
+console.log(numberCollection);
 
 const charactersCollection = new CharactersCollection('ZxYmba');
-const sorter1 = new Sorter(charactersCollection);
-sorter1.sort();
-console.log(sorter1);
+charactersCollection.sort();
+console.log(charactersCollection);
 
 const linkedList = new LinkedList();
 linkedList.add(100);
 linkedList.add(-11);
 linkedList.add(-3);
 linkedList.add(7);
-const sorter2 = new Sorter(linkedList);
-sorter2.sort();
+linkedList.sort();
 linkedList.print();
